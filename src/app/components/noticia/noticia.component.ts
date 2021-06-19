af4b029b34d8c31194f2909e0c0026d454eb6384
Kevin Bueno <kean.solano@gmail.com>
2021-06-18 21:40:35 -0500

    action sheet menu

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-noticia',
@@ -11,7 +12,42 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() index: number;
 
-  constructor() { }
+  constructor(public actionSheetController: ActionSheetController) { }
+
+  abrirNoticia() {
+    console.log('noticia', this.noticia.url);
+  }
+
+  async lanzarMenu() {
+    const actionSheet = await this.actionSheetController.create({
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
 
   ngOnInit() {}
 
